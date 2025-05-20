a21bc7e92d8ec40fc2be968af09d14c7859acd5d
root <root@localhost.localdomain>
2025-05-20 11:43:05 +0800

    upload inspect huawei/h3c devices

---
+++ 1.py
@@ -0,0 +1,42 @@
+import ansible_runner
+
+inventory = """
+[h3c] 
+ansible_host=192.168.1.254 ansible_ssh_user: "admin" ansible_ssh_pass: "admin" ansible_network_os: community.network.ce
+"""
+
+r = ansible_runner.run(
+    private_data_dir='.',  # 当前目录
+    playbook='dis_version.yaml',
+    inventory=inventory,
+    host_pattern="h3c",
+    
+    
+    extravars={
+        'ansible_ssh_user': 'admin',
+        'ansible_ssh_pass': 'admin',
+        'ansible_network_os': 'community.network.ce'
+    }
+)
+
+# print("Status:", r.status)      # e.g. "successful"
+# print("RC:", r.rc)              # e.g. 0
+# print("Stdout:")
+print(r.stdout.read())          # 全部输出
+
+# 如果你想取结果对象
+for each_host_event in r.events:
+    if each_host_event.get("event") == "runner_on_ok":
+        result = each_host_event.get("event_data", {}).get("res", {})
+        if 'stdout' in result:
+            print("纯净stdout：")
+            print("\n".join(result['stdout']))
+
+# import ansible_runner
+
+# inventory = """
+# [default] 
+# localhost ansible_host=192.168.85.171 ansible_port=22 ansible_user=root ansible_password=beikong123
+# """
+
+# r = ansible_runner.run(inventory=inventory, host_pattern="default", module='shell', module_args="cat /proc/cpuinfo | grep processor | wc -l")
---
+++ inspetion_other_info.py
@@ -0,0 +1,25 @@
+import ansible_runner
+import contextlib
+import os
+
+with open(os.devnull, 'w') as fnull:
+    with contextlib.redirect_stdout(fnull):
+        r = ansible_runner.run(
+            private_data_dir='.',
+            playbook='inspetion_other_info.yaml',
+            extravars={
+                'ansible_ssh_user': 'admin',
+                'ansible_ssh_pass': 'admin',
+                'ansible_network_os': 'community.network.ce'
+            },
+        )
+
+for each_host_event in r.events:
+    if each_host_event.get("event") == "runner_on_ok":
+        host = each_host_event.get("event_data", {}).get("host", "unknown")
+        result = each_host_event.get("event_data", {}).get("res", {})
+        if 'stdout' in result:
+            print(f"[{host}] 返回内容：")
+            # 清洗输出：只保留非空行，strip去空格
+            cleaned_lines = [line.strip() for line in result['stdout'] if line.strip()]
+            print("\n".join(cleaned_lines))
