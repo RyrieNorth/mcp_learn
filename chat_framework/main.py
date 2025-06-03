7b0e83a6cfbc2081627fd28f8cc99d5bc5fb6444
NorthSky <2586649501@qq.com>
2025-06-03 10:31:29 +0800

    upload char_framework

---
+++ chat_framework/main.py
@@ -0,0 +1,24 @@
+import os
+from utils import *
+
+def main():
+    history = []
+    while True:
+        try:
+            user_input = input("Input your questions (or 'exit/quit' to exit): >>> ").strip()
+            if user_input.lower() in ("exit", "quit"):
+                print("Bye!")
+                break
+            if user_input:
+                for response in call_llm(prompt=user_input, thinking=True, max_tokens=8192, messages=history, temperature=0.3):
+                    print(response, end="", flush=True)
+                print()
+        except KeyboardInterrupt:
+            print("\nExit.")
+            break
+        except EOFError:
+            print("\nExit.")
+            break
+
+if __name__ == "__main__":
+    main()
