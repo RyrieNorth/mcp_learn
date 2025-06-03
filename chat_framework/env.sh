93c48e386cdd92fec427f251aac8548a91f9cb3e
NorthSky <53655849+RyrieNorth@users.noreply.github.com>
2025-06-03 10:34:50 +0800

    Delete chat_framework directory

--- chat_framework/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export API_KEY=""
-export API_BASE=""
-export MODEL="Qwen3-30B-A3B"
--- chat_framework/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .env_utils import *
-from .llm_client import *
-from .debug import *
-from .logger import *
-from .tools_registry import *
-from .tools_loader import *
