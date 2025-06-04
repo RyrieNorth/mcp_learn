e18c48753b40b1a380316b9031a52426da119f43
NorthSky <2586649501@qq.com>
2025-06-04 15:29:56 +0800

    upload k8s_mcp

--- k8s_mcp/env.sh
+++ k8s_mcp/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export API_KEY="Gzbbn@cloud2025"
-export API_BASE="http://203.88.203.201:8000/v1"
+export API_KEY=""
+export API_BASE=""
 export MODEL="Qwen3-30B-A3B"
-# export API_KEY="sk-iqssxsdotwaywwidqzsehpxvftphkjfvwyuthrdwnokttvce"
+# export API_KEY="sk-****"
 # export API_BASE="https://api.siliconflow.cn"
 # export MODEL="Qwen/Qwen3-30B-A3B"
 
